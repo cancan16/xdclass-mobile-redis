776d2a88968073d3ce4806a187cfd1b560057609
liucancan <916215796@qq.com>
2019-12-20 08:46:15 +0800

    no message

--- src/main/java/com/xdclass/mobile/xdclassmobileredis/XdclassMobileRedisApplication.java
+++ src/main/java/com/xdclass/mobile/xdclassmobileredis/XdclassMobileRedisApplication.java
@@ -1,12 +1,10 @@
 package com.xdclass.mobile.xdclassmobileredis;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableScheduling
 public class XdclassMobileRedisApplication {
 
